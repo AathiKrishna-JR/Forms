98f51ab518bd4c4d259f8a26e808e617352a5bc4
AathiKrishna-JR <aathi3612@gmail.com>
2025-01-30 15:31:09 +0530

    1

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports:[ReactiveFormsModule]
 })
 export class SignupComponent {
   form = new FormGroup({
